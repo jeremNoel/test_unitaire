d268c94eccd4efc14b4870f0af3e5051164f8b23
Jérémie Noël <dev.jnoel@gmail.com>
2019-06-28 16:54:18 +0200

    init tu

---
+++ tests/RoomTest.php
@@ -0,0 +1,15 @@
+<?php
+
+// tests/Util/CalculatorTest.php
+namespace App\Tests\Util;
+
+use App\Entity\Room;
+use PHPUnit\Framework\TestCase;
+
+class RoomTest extends TestCase
+{
+    public function testInitRoom()
+    {
+
+    }
+}
